117ea1cef09921a1ab18c0a5d1e00fcca001374b
thaitam <phithaitam@gmail.com>
2021-03-07 17:13:27 +0700

    nhập xuất thời gian

--- src/QuanLiSach.cpp
+++ src/QuanLiSach.cpp
@@ -1,4 +1,50 @@
+#include<stdio.h>
 #include<stdlib.h>
+typedef struct DateTime{
+    int ngay;
+    int thang;
+    int nam;
+};
+typedef struct Tacgia{
+    char matacgia[30];
+    char tentacgia[30];
+    DateTime ngaysinh;
+};
+typedef struct Nhaxuatban{
+    char manhaxuatban[30];
+    char tennhaxuatban[30];
+};
+// ngay thang
+void nhap(DateTime *time);
+void xuat(DateTime *time);
+typedef struct Sach{
+    char masach[30];
+    char ten[30];
+    Tacgia *tacgia;
+    Nhaxuatban *nhaxuatban;
+    int soluong;
+    float gia;
+};
+typedef struct NguoiMuon{
+    char manguoimuon[30];
+    Sach *sach;
+    
+};
 int main(){
-    return EXIT_SUCCESS;
+    DateTime *ngaysinh;
+    ngaysinh = (DateTime*)malloc(sizeof(DateTime));
+    nhap(ngaysinh);
+    xuat(ngaysinh);
+    return 0;
+}
+void nhap(DateTime *time){
+    printf("ngày:");
+    scanf("%d",&time->ngay);
+    printf("tháng:");
+    scanf("%d",&time->thang);
+    printf("năm: ");
+    scanf("%d",&time->nam);
+}
+void xuat(DateTime *time){
+    printf("%d/%d/%d",time->ngay,time->thang,time->nam);
 }
